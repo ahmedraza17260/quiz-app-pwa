2fbbce375b9d89be2fd32e65e006f8b029847b26
Ahmed Raza <ahmeraza17260@gmail.com>
2020-08-15 11:38:06 +0500

    add api for offline

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,15 +5,48 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 
-firebase.initializeApp({
-    apiKey: "AIzaSyCehShC7k5EUsNKQI2hyDDEqT7bMM0OwJ0",
-    authDomain: "react-quiz-pwa.firebaseapp.com",
-    databaseURL: "https://react-quiz-pwa.firebaseio.com",
-    projectId: "react-quiz-pwa",
-    storageBucket: "react-quiz-pwa.appspot.com",
-    messagingSenderId: "822075472878",
-    appId: "1:822075472878:web:e5029b3b315dc19eb5ecff",
-    measurementId: "G-ZLC44QLC9Y"
-)};
+firebase.initializeApp{
+    (
+        apiKey: "AIzaSyCehShC7k5EUsNKQI2hyDDEqT7bMM0OwJ0",
+        authDomain: "react-quiz-pwa.firebaseapp.com",
+        databaseURL: "https://react-quiz-pwa.firebaseio.com",
+        projectId: "react-quiz-pwa",
+        storageBucket: "react-quiz-pwa.appspot.com",
+        messagingSenderId: "822075472878",
+        appId: "1:822075472878:web:e5029b3b315dc19eb5ecff",
+        measurementId: "G-ZLC44QLC9Y"
+    )
+};
 
 const messaging = firebase.messaging();
+
+
+
+
+
+
+
+
+// const quizCache = 'Quiz-Cache';
+// const assets = [
+//     // 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
+//     'https://opentdb.com/api.php?amount=10&difficulty=easy',
+//     "/static/js / bundle.js",
+//     "/static/js/1.chunk.js",
+//     "/static/js/main.chunk.js",
+//     "/logo192.png",
+//     "/manifest.json",
+//     "/firebase-messaging-sw.js",
+//     "/src/API.ts",
+//     "/index.html",
+//     "/"
+//     // `https://opentdb.com/api.php?amount=${newQuestions.amount}&difficulty=${newQuestions.difficulty}&type=multiple`
+// ];
+
+// this.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(quizCache).then((cache) => {
+//             cache.addAll(assets);
+//         })
+//     )
+// })
--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,56 @@
 const quizCache = 'Quiz-Cache';
 const assets = [
+    // 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
+    // 'https://opentdb.com/api.php?amount=10&difficulty=easy',
     'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple',
+
+    "/static/js / bundle.js",
+    "/static/js/1.chunk.js",
+    "/static/js/main.chunk.js",
+    "/logo192.png",
+    "/manifest.json",
+    "/firebase-messaging-sw.js",
+    "/src/API.ts",
+    "/index.html",
+    "/"
+    // `https://opentdb.com/api.php?amount=${newQuestions.amount}&difficulty=${newQuestions.difficulty}&type=multiple`
 ];
 
-self.addEventListener('install', (event) => {
+this.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(quizCache).then((cache) => {
             cache.addAll(assets);
         })
     )
 })
+
+
+// let cacheData = "quizApp";
+
+// this.addEventListener("install", (event) => {
+//     event.waitUntil(
+//         caches.open(cacheData).then((cache)) => {
+//         cache.addAll([
+//             "/static/js / bundle.js",
+//             "/static/js/1.chunk.js",
+//             "/static/js/main.chunk.js",
+//             "/logo192.png",
+//             "/manifest.json",
+//             "/firebase-messaging-sw.js",
+//             "/src/API.ts",
+//             "/index.html",
+//             "/"
+//         ])
+//     }
+//     )
+// })
+
+// this.addEventListener("fetch", (event) => {
+//     event.responseWith(
+//         caches.match(event.request).then(result) => {
+//         if(result) {
+//             return result
+//         }
+//     }
+//     )
+// })
